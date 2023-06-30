6c0d62cddfc36f1b019a3ab1ec22d93d7dd60d9d
glyzinieh <glyzinie.h@gmail.com>
2023-06-30 12:10:28 +0900

    一括で出力

--- main.py
+++ main.py
@@ -3,18 +3,21 @@ from PIL import Image
 image_path = input("画像のパス:")
 base_file = Image.open(image_path)
 
-z = int(input("ズームレベル:"))
-root = 2**z
+max_zoom = int(input("最大ズームレベル:"))
 
-width = base_file.width / root
-height = base_file.height / root
 
-for x in range(root):
-    sx = width * x
-    ex = sx + width
-    for y in range(root):
-        sy = height * y
-        ey = sy + height
-        base_file.crop((sx, sy, ex, ey)).resize((256, 256)).save(f"{z}_{x}_{y}.png")
+for z in range(max_zoom + 1):
+    root = 2**z
+
+    width = base_file.width / root
+    height = base_file.height / root
+
+    for x in range(root):
+        sx = width * x
+        ex = sx + width
+        for y in range(root):
+            sy = height * y
+            ey = sy + height
+            base_file.crop((sx, sy, ex, ey)).resize((256, 256)).save(f"{z}_{x}_{y}.png")
 
 base_file.close()
