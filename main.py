16857c4279beb0508d1a9de05deea81a18272b4a
glyzinieh <glyzinie.h@gmail.com>
2023-08-06 18:42:30 +0900

    プログレスバーを追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from PIL import Image
+from tqdm import tqdm
 
 
 def bunkatsu(max_zoom, image_path):
@@ -9,21 +10,24 @@ def bunkatsu(max_zoom, image_path):
         os.mkdir(f"tiles/{save_dir}")
 
     base_file = Image.open(image_path)
-    for z in range(max_zoom + 1):
-        root = 2**z
 
-        width = base_file.width / root
-        height = base_file.height / root
+    with tqdm(range(max_zoom + 1)) as zpbar:
+        for z in zpbar:
+            zpbar.set_description("ズームレベル" + str(z))
+            root = 2**z
 
-        for x in range(root):
-            sx = width * x
-            ex = sx + width
-            for y in range(root):
-                sy = height * y
-                ey = sy + height
-                base_file.crop((sx, sy, ex, ey)).resize((256, 256)).save(
-                    f"tiles/{save_dir}/{z}_{x}_{y}.png"
-                )
+            width = base_file.width / root
+            height = base_file.height / root
+
+            for x in range(root):
+                sx = width * x
+                ex = sx + width
+                for y in range(root):
+                    sy = height * y
+                    ey = sy + height
+                    base_file.crop((sx, sy, ex, ey)).resize((256, 256)).save(
+                        f"tiles/{save_dir}/{z}_{x}_{y}.png"
+                    )
     base_file.close()
 
 
@@ -35,5 +39,7 @@ files = [f for f in os.listdir(dir_path) if os.path.splitext(f)[1] == ".png"]
 if not os.path.exists("tiles"):
     os.mkdir("tiles")
 
-for image_path in files:
-    bunkatsu(max_zoom, image_path)
+with tqdm(files) as fpbar:
+    for image_path in fpbar:
+        fpbar.set_description(image_path)
+        bunkatsu(max_zoom, image_path)
